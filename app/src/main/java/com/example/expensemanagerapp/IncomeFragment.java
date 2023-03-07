58197e12ce1f68e0fa9cb969544f5242b282f653
Naiem Arafat <naiemndcsust3775@gmail.com>
2023-03-08 00:14:13 +0600

    added comment

--- app/src/main/java/com/example/expensemanagerapp/IncomeFragment.java
+++ app/src/main/java/com/example/expensemanagerapp/IncomeFragment.java
@@ -35,7 +35,7 @@ import java.util.Date;
  * create an instance of this fragment.
  */
 public class IncomeFragment extends Fragment {
-
+// added comment
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
     private static final String ARG_PARAM1 = "param1";
